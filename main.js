ffd0cce43964157f4e30fbbbb12497acfc97cbe4
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2026-09-16 14:17:50 +0300

    modify css to suit categories

--- main.js
+++ main.js
@@ -3,4 +3,21 @@ const getCategoriesList = async () => {
   return categoriesList.data;
 };
 
-console.log(getCategoriesList());
+const displayCategories = async () => {
+  const categories = await getCategoriesList();
+  const result = categories.map((category) => {
+    return `
+      <li class="category-list">
+        <a href="#" class="category-card" data-category="${category}">${category}</a>
+      </li>
+    `;
+  }); 
+  document.getElementById("hero").innerHTML = result.join("");
+ }
+
+ displayCategories();
+
+const getProductsByCategory = async (category) => {
+  const products = await axios.get(`https://dummyjson.com/products/category/${category}`);
+  return products.data;
+}
